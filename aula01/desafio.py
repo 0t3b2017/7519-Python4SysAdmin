f6738b84c887168325ac98942c53af2aaca521e0
José Roberto da Silva Cruz <roberto@h3b.com.br>
2020-09-22 11:40:51 -0300

    Add: Adding desafio 01 + Simple API

---
+++ aula01/desafio.py
@@ -0,0 +1,56 @@
+"""
+ rot 13 é uma forma clássica de cifrar uma mensagem - consiste em coletar cada caractere
+ de uma mensagem e substituí-lo pelo caractere 13 posições à frente. por exemplo:
+     a letra 'a' seria convertida em 'n' 
+     a letra 'b' seria convertida em 'o'
+     e assim por diante...
+     Considerando o alfabeto que conhecemos e incluindo os algarismos de 0 até 9:
+         - faça uma requisição que colete a mensagem cifrada no seguinte endereço: 
+             -> https://python521.herokuapp.com/desafio
+         - utilizando a lógica do rot13, decodifique a mensagem que está no campo 'data' do json recebido 
+"""
+
+import requests
+
+url = "https://python521.herokuapp.com/desafio"
+
+response = requests.get(url)
+
+rot13 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+print(response.json())
+
+char = response.json()['data']
+
+def rot13_decrypt(char):
+    new_char = ''
+    for c in char.lower():
+        if c == ' ':
+            new_char += ' '
+            continue
+        v = rot13.index(c)
+        new_char += rot13[v - 13]
+    
+    return(new_char)
+
+
+def rot13_encrypt(char):
+    new_char = ''
+    for c in char.lower():
+        if c == ' ':
+            new_char += ' '
+            continue
+        v = rot13.index(c)
+        if (v + 13) > (len(rot13) - 1):
+            new_char += rot13[((v + 13) - (len(rot13) - 1)) - 1]
+            continue
+        new_char += rot13[v + 13]
+    
+    return(new_char)
+
+print(rot13_decrypt(char))
+
+print()
+
+#print(rot13_encrypt(rot13_decrypt(char)))
+print(rot13_encrypt('Quero mais ferias hahaha'))
---
+++ aula01/run.py
@@ -0,0 +1,41 @@
+from flask import Flask, Response, request
+import json
+import desafio
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    retorno = {
+        "app": "Sistema de encrypt / decrypt rot13 - by Jcruz",
+        "version": 1.0
+    }
+
+    return Response(
+        json.dumps(retorno),
+        200,
+        content_type="application/json"
+    )
+
+@app.route("/encrypt", methods=["POST"])
+def encrypt():
+    retorno = request.get_json()
+    msg = retorno['msg']
+    encrypted_msg = desafio.rot13_encrypt(msg)
+    data = {
+        "msg": encrypted_msg
+    }
+    return Response(json.dumps(data), 200, content_type="application/json")
+
+@app.route("/decrypt", methods=["POST"])
+def decrypt():
+    retorno = request.get_json()
+    msg = retorno['msg']
+    decrypted_msg = desafio.rot13_decrypt(msg)
+    data = {
+        "msg": decrypted_msg
+    }
+    return Response(json.dumps(data), 200, content_type="application/json")
+
+if __name__ == "__main__":
+    app.run(debug=True)
